b54e217d2313b61f08df5197e6dc0754116c639a
John Tracy <johnmtracy133@hotmail.com>
2016-09-23 00:31:11 -0400

    footer restruct

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,11 @@
 
 
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-     
+  $(window).scroll(function () {  
       if($(window).scrollTop() > 320){
         $('#myNav').addClass('navbar-fixed-top');
       }
       if($(window).scrollTop() < 321 && $('#myNav').hasClass('navbar-fixed-top')){
         $('#myNav').removeClass('navbar-fixed-top')
       }
-    // if ($(window).scrollTop() > 280) {
-    //   $('#nav_bar').addClass('navbar-fixed');
-    // }
-    // if ($(window).scrollTop() < 281) {
-    //   $('#nav_bar').removeClass('navbar-fixed');
-    //}
   });
+
