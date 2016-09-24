dd34406f68c3400a5f1624fc32e5b98bcd4fcf1f
John Tracy <johnmtracy133@hotmail.com>
2016-09-23 23:42:08 -0400

    para scroll

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,10 @@
 
 
   $(window).scroll(function () {  
-      if($(window).scrollTop() > 320){
+      if($(window).scrollTop() > 355){
         $('#myNav').addClass('navbar-fixed-top');
       }
-      if($(window).scrollTop() < 321 && $('#myNav').hasClass('navbar-fixed-top')){
+      if($(window).scrollTop() < 356 && $('#myNav').hasClass('navbar-fixed-top')){
         $('#myNav').removeClass('navbar-fixed-top')
       }
   });
