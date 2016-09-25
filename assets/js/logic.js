35dea23a3821e733538d65cfe0b650e668b90b46
John Tracy <johnmtracy133@hotmail.com>
2016-09-25 17:04:08 -0400

    random changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,22 @@
 
 
-  $(window).scroll(function () {  
-      if($(window).scrollTop() > 355){
-        $('#myNav').addClass('navbar-fixed-top');
-      }
-      if($(window).scrollTop() < 356 && $('#myNav').hasClass('navbar-fixed-top')){
-        $('#myNav').removeClass('navbar-fixed-top')
-      }
-  });
+  // $(window).scroll(function () {  
+  //     if($(window).scrollTop() > 355){
+  //       $('#myNav').addClass('navbar-fixed-top');
+  //     }
+  //     if($(window).scrollTop() < 356 && $('#myNav').hasClass('navbar-fixed-top')){
+  //       $('#myNav').removeClass('navbar-fixed-top')
+  //     }
+  // });
+var docHeight = $(document).height()
 
+$('#bg-holder').css("height", docHeight);
+
+
+$(window).scroll(function () { 
+
+   $('#bg-holder').css({
+      'top' : -($(this).scrollTop()/3)+"px"
+   }); 
+
+})
